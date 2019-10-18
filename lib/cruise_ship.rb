6ce36313b987b2de97241aa8d45e11f09b4f8c68
Nathan Mousa <nathanmousa1@gmail.com>
2019-10-18 21:04:06 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -7,7 +7,12 @@
 # }
 
 def select_winner(passengers)
-  passengers.each do |suite, passenger|
-    puts "#{suite}: #{passenger}"
+  winner = ""
+  
+  passengers.each do |suite, name|
+    if suite == :suite_a && name.start_with?("A")
+      winner = name
+    end
   end
+  winner
 end
